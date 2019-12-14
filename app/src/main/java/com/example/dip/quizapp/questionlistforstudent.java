bde2e7e2192be22ab9d1bf62f0c2a1ee70f09dbf
diptodeydip <dipdey093@gmail.com>
2019-12-14 16:06:42 +0600

    updated (14-12-19)

--- app/src/main/java/com/example/dip/quizapp/questionlistforstudent.java
+++ app/src/main/java/com/example/dip/quizapp/questionlistforstudent.java
@@ -2,7 +2,7 @@ package com.example.dip.quizapp;
 
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
+//import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
