cc906849755e9939e00ab7b9df98c388a9c0ca43
diptodeydip <dipdey093@gmail.com>
2019-12-24 20:15:15 +0600

    updated (24-12-19)(8:14pm)

--- app/src/main/java/com/example/dip/quizapp/MainActivity.java
+++ app/src/main/java/com/example/dip/quizapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.dip.quizapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,7 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int EXTERNAL_STORAGE_CODE = 1, READ_EXTERNAL_STORAGE=2;
     Intent intent;
     DatabaseReference db;
     //public static String user_email;
@@ -23,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                String[] permissions = {android.Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+                requestPermissions(permissions, EXTERNAL_STORAGE_CODE);
+            }
+        }
 
 /*        db = FirebaseDatabase.getInstance().getReference();
         Classes up = new Classes();
@@ -54,5 +68,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case EXTERNAL_STORAGE_CODE: {
+                if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText(this, "Storage permission is required to store data", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/dip/quizapp/Resultpage.java
+++ app/src/main/java/com/example/dip/quizapp/Resultpage.java
@@ -2,6 +2,7 @@ package com.example.dip.quizapp;
 
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +26,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -32,7 +37,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Resultpage extends AppCompatActivity  {
-
+    Button saveResult;
     RecyclerView rView;
     DatabaseReference db;
     List<resultformat> uploads;
@@ -41,6 +46,7 @@ public class Resultpage extends AppCompatActivity  {
     FirebaseAuth mAuth;
     String userId;
     FirebaseStorage fs;
+    static String data="";
     ValueEventListener dBListener;
 
     @Override
@@ -49,6 +55,14 @@ public class Resultpage extends AppCompatActivity  {
         setContentView(R.layout.activity_resultpage);
 
 
+        saveResult = (Button) findViewById(R.id.saveResult);
+        saveResult.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        saveTotxtFile(MainActivity.classCode.toString(),data);
+                    }
+                }
+        );
         rView = findViewById(R.id.recyclerView3);
         rView.setHasFixedSize(true);
         rView.setLayoutManager(new LinearLayoutManager(this));
@@ -64,7 +78,7 @@ public class Resultpage extends AppCompatActivity  {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 uploads.clear();
-
+                data= "";
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                     resultformat upload = postSnapshot.getValue(resultformat.class);
 
@@ -74,6 +88,12 @@ public class Resultpage extends AppCompatActivity  {
                 //
                 Comparator c = Collections.reverseOrder(new Sortbyreg());
                 Collections.sort(uploads, c);
+                int size = uploads.size();
+                resultformat obj;
+                for (int i=0; i<size; i++) {
+                    obj = uploads.get(i);
+                    data+="Rank: "+Integer.toString(i+1)+"\n Reg No: "+obj.getReg()+"\n Marks: "+obj.getMarks()+"\n\n";
+                }
                 //
                 iAdapter.notifyDataSetChanged();
                 pbar.setVisibility(View.INVISIBLE);
@@ -105,6 +125,27 @@ public class Resultpage extends AppCompatActivity  {
         public int compare(resultformat a, resultformat b)
         {
             return Integer.parseInt(a.marks) - Integer.parseInt(b.marks);
+        }
+    }
+
+
+
+    void saveTotxtFile(String name, String data) {
+        //FILE_NAME = myIp + trIp.getText();
+        // String text = read_msg;
+        try {
+            File path = Environment.getExternalStorageDirectory();
+            File dir = new File(path + "/QuizAPP/");
+            dir.mkdirs();
+            String FILE_NAME = name + ".txt";
+            File file = new File(dir, FILE_NAME);
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(data);
+            bw.close();
+            Toast.makeText(this, FILE_NAME+" is saved in \n" + dir, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+
         }
     }
 }
